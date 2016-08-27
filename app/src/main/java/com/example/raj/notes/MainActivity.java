67e33945869240c5fa21709bda2ebe58e7b77a0b
raj <raj@notes.notes>
2016-08-27 14:01:57 +0530

    initializing

---
+++ app/src/main/java/com/example/raj/notes/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.raj.notes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v)
+            {
+                TextView t1 = (TextView) findViewById(R.id.notifySpace);
+                t1.setText("Authenticating...");
+            }
+        });
+
+    }
+}
